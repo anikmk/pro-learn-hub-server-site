00daf23c53793279e3c47e98dcde3fcce4474c98
anikmollik <anikmollik122321@gmail.com>
2023-11-05 22:11:52 +0600

    show email related course in my posted job (api)

--- index.js
+++ index.js
@@ -41,6 +41,15 @@ async function run() {
     })
 
     // all add job 
+    app.get('/addjobs',async(req,res)=>{
+      console.log(req.query.email)
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await addJobCollection.find(query).toArray();
+      res.send(result)
+    })
     app.post('/addjobs',async(req,res)=>{
       const jobs = req.body;
       console.log(jobs)
