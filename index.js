505c2c8d442876be285f8be40eacb12d674b4016
anikmollik <anikmollik122321@gmail.com>
2023-11-05 17:37:27 +0600

    make dynamic api for single category with four card

--- index.js
+++ index.js
@@ -29,12 +29,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coursesCollection = client.db("proLabHub").collection("courses");
+
+    app.get('/courses/:category',async(req,res)=>{
+      const category = req.params.category;
+      console.log(category)
+      const result = await coursesCollection.find({category}).toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
