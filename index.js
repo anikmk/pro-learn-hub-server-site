960f4a33ac448406d2f8878e4670641b0dafbd16
anikmollik <anikmollik122321@gmail.com>
2023-11-07 12:15:33 +0600

    data fatching error handleing

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const coursesCollection = client.db("proLabHub").collection("courses");
     const addJobCollection = client.db("proLabHub").collection("addJobs");
@@ -104,8 +104,8 @@ async function run() {
       res.send(result)
     })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
